eef13a7d5442d6d057a02db948de26165409cbe7
gau820827 <gau820827@gmail.com>
2016-11-10 17:01:44 +0800

    Remove duplicate function in util.py

--- util.py
+++ util.py
@@ -26,23 +26,3 @@ def infoDisplay(counter, ipcounter, tcpcounter, udpcounter, httpcounter):
     print "Total number of tcp packets: ", tcpcounter
     print "Total number of udp packets: ", udpcounter
     print "Total number if http requests: ", httpcounter
-
-
-def loadIntoVector(feature):
-    """Load the extraction payload into a 1500 columns vector
-    
-    @param feature: the extraction payload, for instance GET HTTP1.1
-
-    @returns: A 1500 columns vector
-    """
-
-
-    datas = [ x for x in feature.split(",") if x != ""]
-    if len(datas) > MTU:
-        datas = datas[:MTU]
-    shift = (MTU - len(datas))/2
-    vector = 1500*["0"]
-    for data in datas:
-        vector[shift] = str(data)
-        shift += 1
-    return vector
