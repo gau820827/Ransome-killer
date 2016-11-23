4e983879b7509b1fde527787c59c8fbd88cc0082
gau820827 <gau820827@gmail.com>
2016-11-23 18:30:34 +0800

    Add payloads parser in pcap_Parser.py

--- pcap_Parser.py
+++ pcap_Parser.py
@@ -1,5 +1,6 @@
 """ Capture all packets in malware_pcap and retrieve the malicious parts """
 
+import sys
 import os
 import dpkt
 
@@ -8,11 +9,13 @@ from settings import *
 from util import *
 
 
-def getPayload(filename, outputName):
+def getPayload(filename, outputName, mode=1):
     """Get payload according to some constrains.
     @parameter   filename: The pcap file you want to extract
                outputName: Write to this file
-
+                     mode: The capture mode, 1 for http req
+                                             2 for payloads
+    
     @returns: None
     """
 
@@ -40,29 +43,32 @@ def getPayload(filename, outputName):
         tcp = ip.data
         
         
-        """ Get normal TCP payload """
-        # if len(tcp.data) > 0 and tcp.dport != 80:
-        #     outputFilep.write(' '.join(format(ord(x), 'b') for x in tcp.data) + '\n')
+        if mode == 2:
+            """ Get normal TCP payload """
+            if len(tcp.data) > 0 and tcp.dport != 80:
+                print "packet {}: {}".format(ipcounter, len(tcp.data))
+                outputFile.write(','.join(format(ord(x)) for x in tcp.data) + '\n')
 
         
-        """ Get http header """
-        if tcp.dport == 80 and len(tcp.data) > 0:
-            try:
-                http = dpkt.http.Request(tcp.data)
-            except dpkt.NeedData:
-                extraction = handleErrorPacket(tcp.data)
-                print "packet {}: Missing Data {}".format(ipcounter, extraction)
-                outputFile.write(",".join(format(ord(x)) for x in extraction) + '\n')
-            except dpkt.UnpackError:
-                ''' Invalid request such as some HTML fragments, not for use now '''
-                
-                print "packet " + str(ipcounter) + ": Invalid Request " + tcp.data
-                # outputFilep.write(' '.join(format(ord(x), 'b') for x in tcp.data) + '\n')
-            else:
-                print "packet " + str(ipcounter) + ": " + http.method + http.uri
-                outputFile.write(",".join(format(ord(x)) for x in tcp.data) + '\n')
+        if mode == 1:
+            """ Get http header """
+            if tcp.dport == 80 and len(tcp.data) > 0:
+                try:
+                    http = dpkt.http.Request(tcp.data)
+                except dpkt.NeedData:
+                    extraction = handleErrorPacket(tcp.data)
+                    print "packet {}: Missing Data {}".format(ipcounter, extraction)
+                    outputFile.write(",".join(format(ord(x)) for x in extraction) + '\n')
+                except dpkt.UnpackError:
+                    ''' Invalid request such as some HTML fragments, not for use now '''
+                    
+                    print "packet " + str(ipcounter) + ": Invalid Request " + tcp.data
+                    # outputFilep.write(' '.join(format(ord(x), 'b') for x in tcp.data) + '\n')
+                else:
+                    print "packet " + str(ipcounter) + ": " + http.method + http.uri
+                    outputFile.write(",".join(format(ord(x)) for x in tcp.data) + '\n')
 
-        # print "packet " + str(ipcounter) + " = " + str(len(tcp.data))
+            # print "packet " + str(ipcounter) + " = " + str(len(tcp.data))
 
     outputFile.close()
     print  "Total number of packets in the pcap file: ", ipcounter
@@ -70,6 +76,19 @@ def getPayload(filename, outputName):
 
 if __name__ == '__main__':
     
+
+    """ Mode 1 to capture http request
+        Mode 2 to capture tcp payloads
+        Default is Mode 1
+    """
+    mode = 1
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "-p":
+            mode = 2
+        else:
+            print "Invalid argument, Use Default settings......"
+
+
     """ First we handle the malware pcap """
     trainFile = open(inputMalName[0], "w")
     testFile = open(inputMalName[1], "w")
@@ -80,7 +99,7 @@ if __name__ == '__main__':
         for file in os.listdir(dirMalName[catagory]):
             filepath = os.path.join(dirMalName[catagory], file)
             if ".pcap" in file:
-                getPayload(filepath, inputMalName[catagory])
+                getPayload(filepath, inputMalName[catagory], mode)
 
 
     trainFile.close()
@@ -97,7 +116,7 @@ if __name__ == '__main__':
         for file in os.listdir(dirNorName[catagory]):
             filepath = os.path.join(dirNorName[catagory], file)
             if ".pcap" in file:
-                getPayload(filepath, inputNorName[catagory])
+                getPayload(filepath, inputNorName[catagory], mode)
 
 
     trainFile.close()
